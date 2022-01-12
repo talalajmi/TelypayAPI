c555fdd439035c8c6389a99652c081236354e7f2
Talal Al Ajmi <talalajmi98@egmail.com>
2022-01-12 22:31:55 +0400

    API Ready

---
+++ TelypayAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TelypayAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+
+        [HttpPost]
+        public IActionResult checkUser([FromQuery] string username, [FromQuery] string password)
+        {
+            if (username == "telypay" && password == "password123")
+            {
+                return StatusCode(200, "Success");
+            }
+            else if (username == "telypay" && password == "wrong-password")
+            {
+                return StatusCode(400, "Wrong Password");
+            }
+            else if (username == "error" && password == "error")
+            {
+                return StatusCode(500, "Error");
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
